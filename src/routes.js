58ae140c76324df6d9d0b9d35b915e562ecfb6af
Luiz Moura <luiz.oliveiramoura@hotmail.com>
2020-08-31 14:17:20 -0400

    feat: created UserController.js

--- src/routes.js
+++ src/routes.js
@@ -1,16 +1,9 @@
 import { Router } from 'express';
-import User from './app/models/User';
+
+import UserController from './app/controllers/UserController';
 
 const routes = new Router();
 
-routes.get('/', async (request, response) => {
-  const user = await User.create({
-    name: 'Luiz Moura',
-    email: 'luizhom@Outlook.com',
-    password_hash: '12345678',
-  });
-
-  return response.json(user);
-});
+routes.post('/users', UserController.store);
 
 export default routes;
