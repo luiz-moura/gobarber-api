60f117e7a6688fe8b04850e692ec229fefebdbf8
Luiz Moura <luiz.oliveiramoura@hotmail.com>
2020-09-12 17:14:38 -0400

    feat: appointment controller and date_fns

---
+++ src/app/controllers/AppointmentController.js
@@ -0,0 +1,102 @@
+import * as Yup from 'yup';
+import { startOfHour, isBefore, parseISO } from 'date-fns';
+import User from '../models/User';
+import File from '../models/File';
+import Appointment from '../models/Appointment';
+
+class AppointmentController {
+  async index(request, response) {
+    const { page = 1 } = request.query;
+
+    const appointments = await Appointment.findAll({
+      where: {
+        user_id: request.userId,
+        canceled_at: null,
+      },
+      order: ['date'],
+      attributes: ['id', 'date'],
+      limit: 20,
+      offset: (page - 1) * 20,
+      include: [
+        {
+          model: User,
+          as: 'provider',
+          attributes: ['id', 'name'],
+          includes: [
+            {
+              model: File,
+              as: 'avatar',
+              attributes: ['id', 'path', 'url'],
+            },
+          ],
+        },
+      ],
+    });
+
+    return response.json(appointments);
+  }
+
+  async store(request, response) {
+    const schema = Yup.object().shape({
+      provider_id: Yup.number().required(),
+      date: Yup.date().required(),
+    });
+
+    if (!(await schema.isValid(request.body))) {
+      return response.status(400).json({ error: 'Validation fails' });
+    }
+
+    const { provider_id, date } = request.body;
+
+    /**
+     * Check if provider_id is a provider
+     */
+    const checkIsProvider = await User.findOne({
+      where: { id: provider_id, provider: true },
+    });
+
+    if (!checkIsProvider) {
+      return response
+        .status(401)
+        .json({ error: 'You can only create appointments with providers' });
+    }
+
+    /**
+     * Check for past dates
+     */
+    const hourStart = startOfHour(parseISO(date));
+
+    if (isBefore(hourStart, new Date())) {
+      return response
+        .status(400)
+        .json({ error: 'Past dates are not permitted' });
+    }
+
+    /**
+     * Check date availability
+     */
+    const checkAvailability = await Appointment.findOne({
+      where: {
+        provider_id,
+        canceled_at: null,
+        date: hourStart,
+      },
+    });
+
+    if (checkAvailability) {
+      return response
+        .status(400)
+        .json({ error: 'Appointment date is not available' });
+    }
+
+    const appointment = await Appointment.create({
+      user_id: request.userId,
+      provider_id,
+      date: hourStart,
+    });
+
+    return response.json(appointment);
+  }
+}
+
+export default new AppointmentController();
